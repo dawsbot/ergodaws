c89c00d1dd74579f4c8bdc20cb6d345a7ca0ffec
Dawson Botsford <DawsonBotsford@gmail.com>
2016-09-05 11:42:34 -0700

    lots of updates

--- keymap.c
+++ keymap.c
@@ -1,6 +1,3 @@
-// Netable differences vs. the default firmware for the ErgoDox EZ:
-// 1. The Cmd key is now on the right side, making Cmd+Space easier.
-// 2. The media keys work on OSX (But not on Windows).
 #include "ergodox.h"
 #include "debug.h"
 #include "action_layer.h"
@@ -17,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|  LGui  |
+ * |Ctrl/Esc| A/L1 |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -27,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 |Bakspc| Enter|------|       |------| RShift |Space |
+ *                                 |Bakspc| Enter|------|       |------| Enter  |Space |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -35,23 +32,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,           KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,           KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        CTL_T(KC_ESC),  LT(SYMB, KC_A), KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),    KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,        LALT(KC_LSFT),  KC_LEFT,KC_LGUI,
-                                                ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                                 KC_BSPC,KC_ENT,KC_END,
+        KC_EQL,         KC_1,           KC_2,   KC_3,   KC_4,      KC_5,   KC_LEFT,
+        KC_TAB,         KC_Q,           KC_W,   KC_E,   KC_R,      KC_T,   TG(SYMB),
+        CTL_T(KC_ESC),  LT(SYMB, KC_A), KC_S,   KC_D,   KC_F,      KC_G,
+        KC_LSFT,        CTL_T(KC_Z),    KC_X,   KC_C,   KC_V,      KC_B,   ALL_T(KC_NO),
+        LT(SYMB,KC_GRV),KC_QUOT,        LALT(KC_LSFT),  KC_LEFT,   KC_LGUI,
+                                                    ALT_T(KC_APP), KC_LGUI,
+                                                                   KC_HOME,
+                                                KC_BSPC,KC_ENT,    KC_END,
         // right hand
         KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
         TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                     KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_LGUI,
+                     KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
         MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                              KC_UP,  KC_DOWN,KC_LEFT,KC_RIGHT,          KC_FN1,
-        KC_LALT,        CTL_T(KC_ESC),
+        KC_LALT,  CTL_T(KC_ESC),
         KC_PGUP,
-        KC_PGDN,KC_RSFT, KC_SPC
+        KC_PGDN,  KC_ENT,  KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -163,17 +160,10 @@ void matrix_init_user(void) {
     ergodox_right_led_1_on();
     ergodox_right_led_2_on();
     ergodox_right_led_3_on();
-    wait_ms(600);
-    ergodox_right_led_1_on();
-    ergodox_right_led_2_on();
-    ergodox_right_led_3_off();
-    wait_ms(600);
-    ergodox_right_led_1_on();
-    ergodox_right_led_2_off();
-    wait_ms(600);
+    wait_ms(500);
 };
 
-// Runs constantly in the background, in a loop.
+// Runs constantgly in the background, in a loop.
 void matrix_scan_user(void) {
     uint8_t layer = biton32(layer_state);
 
@@ -183,10 +173,11 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
         case 1:
-            ergodox_right_led_2_on();
+            ergodox_right_led_1_on();
             break;
         case 2:
-            ergodox_right_led_3_on();
+            ergodox_right_led_1_on();
+            ergodox_right_led_2_on();
             break;
         default:
             // none
